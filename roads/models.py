aa77b079d6f1996915457d222a6289f191835dc3
arijitiiest <1999arijitchoudhury@gmail.com>
2020-07-20 14:57:39 +0530

    model changes

--- roads/models.py
+++ roads/models.py
@@ -6,6 +6,10 @@ class Road(models.Model):
     road_name = models.CharField(max_length=100)
     road_condition = models.CharField(max_length=600, blank=True)
     image = models.FileField(blank=False, null=False)
+    latitude = models.DecimalField(
+        max_digits=22, decimal_places=16, blank=True, null=True)
+    longitude = models.DecimalField(
+        max_digits=22, decimal_places=16, blank=True, null=True)
     owner = models.ForeignKey(
         User, related_name='roads', on_delete=models.CASCADE, null=True)
     created_at = models.DateTimeField(auto_now_add=True)

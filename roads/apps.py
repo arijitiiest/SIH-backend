0538f51270f578b7f2e6e78dbc426eedb187a777
arijitiiest <1999arijitchoudhury@gmail.com>
2020-07-31 22:49:08 +0530

    some changes

--- roads/apps.py
+++ roads/apps.py
@@ -1,4 +1,7 @@
 from django.apps import AppConfig
+from _io import BytesIO
+from django.core.files.base import ContentFile
+from django.core.files.storage import default_storage
 
 
 class RoadsConfig(AppConfig):
@@ -7,5 +10,8 @@ class RoadsConfig(AppConfig):
     @staticmethod
     def predict(image):
         # ML Model
-
-        return image
+        file = ContentFile(image.read())
+        path = default_storage.save(
+            'model/' + str(image), ContentFile('media/user/Screenshot_from_2020-06-06_23-45-11.png'))
+        print(path)
+        return path
